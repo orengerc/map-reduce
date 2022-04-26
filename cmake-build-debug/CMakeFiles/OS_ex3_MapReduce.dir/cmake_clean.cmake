42d42caf1456b2deaee23b56f0a1f4d7eaf1c3cd
Oren Gercenshtei <oren_gerc@cs.huji.ac.il>
2022-04-26 08:45:46 +0300

    build clion environment for the project

---
+++ cmake-build-debug/CMakeFiles/OS_ex3_MapReduce.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS_ex3_MapReduce.dir/main.cpp.o"
+  "OS_ex3_MapReduce"
+  "OS_ex3_MapReduce.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OS_ex3_MapReduce.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
